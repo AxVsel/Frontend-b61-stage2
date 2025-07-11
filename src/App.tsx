5671117d865444c2599266e383784696a349f0c8
Muh. Yaumil Aksah Hamid <assperacus77@gmail.com>
2025-07-11 09:39:10 +0800

    day4-checkpoint1

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,78 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Posts from "./pages/Posts";
-import PostDetail from "./pages/PostDetail";
+import Products from "./pages/Products";
+import Dashboard from "./pages/DashBoard";
+import Login from "./pages/login";
+import { Button } from "./components/ui/button";
+import { AuthProvider } from "./context/AuthProvider";
+import PrivateRoute from "./lib/PrivateRoute";
+import ThemeToggle from "./lib/ThemeToggle";
+import { useAuth } from "./hooks/useAuth";
 
-export default function App() {
+function Header() {
+  const { token, logout } = useAuth();
+
   return (
-    <BrowserRouter>
-      <div className="w-full flex gap-4 p-4 justify-center border-b mb-8">
-        <Button asChild variant="outline">
-          <Link to="/">Home</Link>
+    <div className="w-full flex flex-wrap gap-4 p-4 border-b mb-4 bg-white dark:bg-zinc-900">
+      <Button className=" hover:bg-zinc-600" asChild variant="outline">
+        <Link className="" to="/">
+          Home
+        </Link>
+      </Button>
+      <Button className=" hover:bg-zinc-600" asChild variant="outline">
+        <Link to="/about">About</Link>
+      </Button>
+
+      {token && (
+        <Button className=" hover:bg-zinc-600" asChild variant="outline">
+          <Link to="/products">Products</Link>
         </Button>
-        <Button asChild variant="outline">
-          <Link to="/about">About</Link>
+      )}
+
+      {token && (
+        <Button className=" hover:bg-zinc-600" asChild variant="outline">
+          <Link to="/dashboard">Dashboard</Link>
         </Button>
-        <Button asChild variant="outline">
-          <Link to="/posts">Posts</Link>
-        </Button>
+      )}
+      <div className="float-right">
+        <ThemeToggle />
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />}>
-          <Route path=":id" element={<PostDetail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      {token ? (
+        <Button onClick={logout} variant="destructive">
+          Logout
+        </Button>
+      ) : (
+        <Button className=" hover:bg-zinc-600" asChild variant="outline">
+          <Link to="/login">Login</Link>
+        </Button>
+      )}
+    </div>
   );
 }
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/products"
+            element={
+              <PrivateRoute>
+                <Products />
+              </PrivateRoute>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/DashBoard.tsx
@@ -0,0 +1,8 @@
+export default function PostDetail() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-primary">Dashboard page</h1>
+      <p className="text-gray-600 mt-2">Welcome dashboard Page</p>
+    </div>
+  );
+}
